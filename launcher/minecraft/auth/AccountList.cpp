06a61f5c80b7252c010b5e070c0c8ad024bf1648
Dmitriy Kholkin <ataraxiadev@ataraxiadev.com>
2025-05-03 16:14:32 +0300

    auto sync and build releases

--- launcher/minecraft/auth/AccountList.cpp
+++ launcher/minecraft/auth/AccountList.cpp
@@ -599,12 +599,13 @@ void AccountList::setListFilePath(QString path, bool autosave)
 
 bool AccountList::anyAccountIsValid()
 {
-    for (auto account : m_accounts) {
-        if (account->ownsMinecraft()) {
-            return true;
-        }
-    }
-    return false;
+    // for (auto account : m_accounts) {
+    //     if (account->ownsMinecraft()) {
+    //         return true;
+    //     }
+    // }
+    // return false;
+    return true;
 }
 
 void AccountList::fillQueue()
